60c975441a344c89563d83b8d39dae9f7bb6cca6
Andrea Riley <andreariley@me.com>
2023-10-26 19:02:41 -0400

    refactored with functions

--- js/app.js
+++ js/app.js
@@ -1,95 +1,82 @@
 'use strict';
 
 window.onload = function() {
-  let userName = prompt('Hi there! What\'s your name?');
-  let greeting = document.getElementById('greeting');
-  greeting.innerHTML = `Hello, <span id="userNameColor">${userName}</span>! I'm glad you're here. Let's play a game to get to know me better!`;
-
-  let questions = [
-    { q: 'Was I born in Silicon Valley?', a: 'NO' },
-    { q: 'Did I study at UC Davis?', a: 'YES' },
-    { q: 'Have I ever served in the military?', a: 'YES' },
-    { q: 'Is my goal to become a famous singer?', a: 'NO' },
-    { q: 'Do I want to move my family to Spain?', a: 'YES' }
-  ];
+  const userName = prompt('Hi there! What\'s your name?');
+  const greeting = document.getElementById('greeting');
+  greeting.innerHTML = `Hello, <span id="userNameColor">${userName}</span>! I'm glad you're here. Let's play a game to get to know me better.`;
 
   let correctAnswers = 0;
 
-  for (let i = 0; i < questions.length; i++) {
-    let answer = prompt(questions[i].q + ' (yes/no)').toUpperCase();
-    
-    if (answer === 'Y' || answer === 'YES' || answer === 'N' || answer === 'NO') {
-      if (answer === questions[i].a || answer[0] === questions[i].a[0]) {
-        correctAnswers++;
-        alert('Correct!');
-      } else {
-        alert('Wrong!');
-      }
+  function askYesNoQuestion(question, correctAnswer) {
+    const answer = prompt(question + ' (yes/no)').toUpperCase();
+    if (answer === correctAnswer) {
+      correctAnswers++;
+      alert('Correct!');
+    } else if (answer === 'Y' || answer === 'N' || answer === 'YES' || answer === 'NO') {
+      alert('Wrong!');
     } else {
       alert('I don\'t think so! I accept only y/n or yes/no.');
     }
   }
 
-  let secretNumber = 8;
-  let attemptsForSixth = 4;
-  let userGuessedCorrectly = false;
+  const questions = [
+    'Was I born in Silicon Valley?',
+    'Did I study at UC Davis?',
+    'Have I ever served in the military?',
+    'Is my goal to become a famous singer?',
+    'Do I want to move my family to Spain?'
+  ];
+  const answers = ['NO', 'YES', 'YES', 'NO', 'YES'];
 
-  for (let i = 0; i < attemptsForSixth; i++) {
-    let userGuess = parseInt(prompt('Guess a number between 1 and 10:'));
-    
-    if (userGuess === secretNumber) {
-      alert('Congratulations! You guessed the correct number.');
-      userGuessedCorrectly = true;
-      correctAnswers++;
-      break;
-    } else if (userGuess < secretNumber) {
-      alert('Too low! Try again.');
-    } else if (userGuess > secretNumber) {
-      alert('Too high! Try again.');
+  questions.forEach((question, index) => {
+    askYesNoQuestion(question, answers[index]);
+  });
+
+  const secretNumber = 8;
+  const attemptsForSixth = 4;
+
+  function guessNumber(secretNumber, maxAttempts) {
+    for (let i = 0; i < maxAttempts; i++) {
+      const userGuess = parseInt(prompt('Guess a number between 1 and 10:'));
+      if (userGuess === secretNumber) {
+        correctAnswers++;
+        alert('Congratulations! You guessed the correct number.');
+        return;
+      } else if (userGuess < secretNumber) {
+        alert('Too low! Try again.');
+      } else {
+        alert('Too high! Try again.');
+      }
     }
-  }
-
-  if (!userGuessedCorrectly) {
     alert(`Sorry, you've used all your attempts. The correct number was ${secretNumber}.`);
   }
 
-  let possibleAnswers = ["THAILAND", "CANADA", "MEXICO", "SPAIN", "JAPAN", "FRANCE", "GERMANY", "ENGLAND", "SCOTLAND", "IRELAND", "FIJI", "ITALY", "PORTUGAL", "BELGIUM" ];
-  let attemptsForSeventh = 6;
-  let hasGuessedCorrectly = false;
+  guessNumber(secretNumber, attemptsForSixth);
 
-  for (let i = 0; i < attemptsForSeventh; i++) {
-    let userGuess = prompt("Which countries have I visited?").toUpperCase();
-    
-    let found = false;
-    for (let j = 0; j < possibleAnswers.length; j++) {
-      if (userGuess === possibleAnswers[j]) {
-        found = true;
-        break;
+  const possibleAnswers = ["THAILAND", "CANADA", "MEXICO", "SPAIN", "JAPAN", "FRANCE", "GERMANY", "ENGLAND", "SCOTLAND", "IRELAND", "FIJI", "ITALY", "PORTUGAL", "BELGIUM"];
+  const attemptsForSeventh = 6;
+
+  function guessCountry(possibleAnswers, maxAttempts) {
+    for (let i = 0; i < maxAttempts; i++) {
+      const userGuess = prompt("Which countries have I visited?").toUpperCase();
+      if (possibleAnswers.includes(userGuess)) {
+        correctAnswers++;
+        alert("Correct! I have visited " + userGuess + ".");
+        return;
+      } else {
+        alert("Wrong guess. Try again!");
       }
     }
-
-    if (found) {
-      alert("Correct! I have visited " + userGuess + ".");
-      correctAnswers++;
-      hasGuessedCorrectly = true;
-      break;
-    } else {
-      alert("Wrong guess. Try again!");
-    }
-  }
-
-  if (!hasGuessedCorrectly) {
-    let countriesString = '';
-    for (let i = 0; i < possibleAnswers.length; i++) {
-      countriesString += possibleAnswers[i];
-      if (i !== possibleAnswers.length - 1) {
-        countriesString += ', ';
-      }
-    }
+    const countriesString = possibleAnswers.join(', ');
     alert(`Sorry, you've used all your attempts. The countries I've visited are: ${countriesString}.`);
   }
 
+  guessCountry(possibleAnswers, attemptsForSeventh);
+
   alert(`Thanks for playing, ${userName}! You got ${correctAnswers} out of 7 questions right. That's ${((correctAnswers / 7) * 100).toFixed(2)}% correct!`);
 };
+
+
+
 
 
